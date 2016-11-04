36adb6622e564d06c228b747f6421034dfd245e4
Mattias Gees <mattias.gees@gmail.com>
2016-11-04 15:11:23 +0100

    Fix the not working export

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-sudo export DEBIAN_FRONTEND=noninteractive
-
 sudo apt-get -q update
-sudo apt-get -y upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 #sudo apt-get -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 sudo apt-get -y autoremove

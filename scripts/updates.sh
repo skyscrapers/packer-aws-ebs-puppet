12c8dd412de9da208cba8f819cadec1118b7f6ee
Philip Dubois <heazky@gmail.com>
2017-01-20 15:58:16 +0100

    Adding sleep in update script

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get -q update
+sleep 5
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 #sudo apt-get -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 sudo apt-get -y autoremove

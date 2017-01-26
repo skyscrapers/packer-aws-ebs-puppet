a77faf87a3978e122168e85cb62db9015b7da77e
Philip Dubois <heazky@gmail.com>
2017-01-26 08:26:07 +0100

    Add some comment about aws-sdk version

--- scripts/puppet_install.sh
+++ scripts/puppet_install.sh
@@ -7,6 +7,9 @@ echo " - Done"
 sudo /usr/bin/logger -t autobootstrap "installed puppet"
 echo "* installing gems"
 sudo gem install hiera-eyaml
+# aws-codedeploy-agent 1.0-1.1067 depends on aws-sdk-core ~2.6.11 and will ERROR on newer versions (~2.7).
+# We'll probably have to update again once they release the newest version with commit:
+# https://github.com/aws/aws-codedeploy-agent/commit/50db2ec9013cfe8f1a857de53c806d6c67d8d07b
 sudo gem install aws-sdk -v '~> 2.6.11'
 sudo gem install hiera-eyaml-kms
 echo " - Done"

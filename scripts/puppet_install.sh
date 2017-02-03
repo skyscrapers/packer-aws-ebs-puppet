8208a52ae14e7ac42c1c780c14733efdc4fcb5ab
Simon Rondelez <simonrondelez@me.com>
2017-02-03 15:59:35 +0100

    permission issue patch

--- scripts/puppet_install.sh
+++ scripts/puppet_install.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 eval `cat /etc/lsb-release`
-sudo wget -qO - https://apt.puppetlabs.com/pubkey.gpg | apt-key add -
+sudo wget -qO - https://apt.puppetlabs.com/pubkey.gpg | sudo apt-key add -
 echo -n "* added puppetlabs apt key"
-sudo echo "deb https://apt.puppetlabs.com/ $(lsb_release -c -s) PC1" >> /etc/apt/sources.list.d/puppet.list
+sudo echo "deb https://apt.puppetlabs.com/ $(sudo lsb_release -c -s) PC1" | sudo tee -a /etc/apt/sources.list.d/puppet.list
 echo -n "* added puppetlabs apt repo"
 echo -n "* Executing apt-get update"
 sudo apt-get update
 echo -n "* Attempting to install puppet"
 sudo apt-get install -y --force-yes puppet-agent=1.8.*
 echo " - Done"
+sudo ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
 sudo /usr/bin/logger -t autobootstrap "installed puppet"
 echo "* installing gems"
-sudo gem install hiera-eyaml
+sudo /opt/puppetlabs/puppet/bin/gem install hiera-eyaml
 # aws-codedeploy-agent 1.0-1.1067 depends on aws-sdk-core ~2.6.11 and will ERROR on newer versions (~2.7).
 # We'll probably have to update again once they release the newest version with commit:
 # https://github.com/aws/aws-codedeploy-agent/commit/50db2ec9013cfe8f1a857de53c806d6c67d8d07b
-sudo gem install aws-sdk -v '~> 2.6.11'
-sudo gem install hiera-eyaml-kms
+sudo /opt/puppetlabs/puppet/bin/gem install aws-sdk -v '~> 2.6.11'
+sudo /opt/puppetlabs/puppet/bin/gem install hiera-eyaml-kms
 echo " - Done"
 sudo /usr/bin/logger -t autobootstrap "installed puppet gems"
